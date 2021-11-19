7dc88eaf4c6b4113a7b7eef409252172e68f3d9d
Kainaris <adiazproven@gmail.com>
2021-11-19 14:02:22 +0100

    add: exampleSchema

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,37 +1,46 @@
 // https://codesandbox.io/s/github/tchaumeny/formik-schema/tree/master/demo?file=/src/forms.jsx:1798-1804
 // https://github.com/tchaumeny/formik-schema
 
-import FormikBuilder from "../../components/FormikBuilder";
+import FormikBuilder, { FormSchema } from "../../components/FormikBuilder";
 
-  // TODO coger datos iniciales desde el servidor cuando lo tengamos
-  const initialValues = {
-    firstName: "Lamarr"
-  }
+// Form Schemas will be stored in a separate file
+const exampleSchema: FormSchema = {
+  fields: [
+    {
+      name: "firstName",
+      label: "First Name: ",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "secondName",
+      label: "Second Name: ",
+      type: "text",
+      required: false,
+    },
+  ],
+};
+
+// TODO coger datos iniciales desde el servidor cuando lo tengamos
+const exampleData = {
+  firstName: "Lamarr",
+};
+
+// TODO intl ?
+const errorMessageRequired = "Required";
 
 const App = () => {
-  return (<>
-    <h1>FORMULARIO</h1>
-    <FormikBuilder
-      fields={[
-        {
-          name: 'firstName',
-          label: 'First Name: ',
-          type: 'text',
-          required: true,
-        },
-        {
-          name: 'secondName',
-          label: 'Second Name: ',
-          type: 'text',
-          required: false,
-        }
-      ]}
-      errorMessageRequired="Required"
-      initialValues={initialValues}
-      onSubmit={(values) => console.log(values)}
-    />
-  </>
-);
-}
+  return (
+    <>
+      <h1>FORMULARIO</h1>
+      <FormikBuilder
+        schema={exampleSchema}
+        errorMessageRequired={errorMessageRequired}
+        initialValues={exampleData}
+        onSubmit={(values) => console.log(values)}
+      />
+    </>
+  );
+};
 
 export default App;
