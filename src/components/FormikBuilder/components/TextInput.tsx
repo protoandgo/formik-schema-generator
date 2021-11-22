db1a0ba09a2a6753b7624d29b1bdab080862a62f
Kainaris <adiazproven@gmail.com>
2021-11-22 15:33:24 +0100

    add: started the onBlur function generations

---
+++ src/components/FormikBuilder/components/TextInput.tsx
@@ -0,0 +1,27 @@
+import { Input } from "antd";
+import { useField } from "formik";
+
+type TextInputProps = {
+  [x: string]: any;
+  name: string;
+  onBlur: (() => void);
+  visible: boolean;
+}
+const TextInput = (props: TextInputProps) => {
+  const [field, meta] = useField(props);
+  // console.log(props.name + " visible? " + props.visible);
+  if (props.onBlur) props.onBlur();
+  return (
+    <div hidden={props.visible!= null && !props.visible}>
+      <label htmlFor={props.id || props.name}>{props.label}</label>
+      <Input {...field} onBlur={e => {
+        field.onBlur(e);
+        if (props.onBlur) props.onBlur();
+      }} />
+      {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+      <br />
+    </div>
+  );
+};
+
+export default TextInput;
