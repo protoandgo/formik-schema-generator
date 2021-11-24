ec220cc736722f8f3391dcd90a674b952994a694
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-11-24 16:36:19 +0100

    add NumberInput component

---
+++ src/components/FormikBuilder/components/NumberInput.tsx
@@ -0,0 +1,26 @@
+import { InputNumber, Typography } from 'antd';
+import { useField } from 'formik';
+
+type NumberInputProps = {
+    [x: string]: any;
+    name: string;
+    label: string;
+}
+
+const NumberInput = (props: NumberInputProps) => {
+    const [field, meta, label] = useField(props);
+
+    return (
+      <>
+        <label>
+          <Typography>{label}</Typography>
+        </label>
+        <InputNumber {...field} {...props}>0</InputNumber>
+        {meta.touched && meta.error ? (
+          <Typography style={{ color: "darkred" }}>{meta.error}</Typography>
+        ) : null}
+        <br />
+      </>
+    );
+}
+
