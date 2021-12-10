476f1849f4e2d67466059a37438e3d046a9e7a0b
Sebuntu <sebastiangomezzz@gmail.com>
2021-12-10 10:59:36 +0100

    add formik props to the addInput.tsx component

--- src/components/FormikBuilderComponents/materialUI/AddInput.tsx
+++ src/components/FormikBuilderComponents/materialUI/AddInput.tsx
@@ -1,12 +1,20 @@
 import React, { useRef, useState } from "react";
-// MUI IMPORTS 
+// Basic components
+import {FieldLabel, RedErrorBelow} from './BasicComponents'
+// MUI IMPORTS
 import { Box } from "@mui/system";
-import { Stack, Typography, Chip, TextField, TextFieldProps } from "@mui/material";
-import { Cancel } from '@mui/icons-material';
+import { Stack, Typography, TextField, TextFieldProps } from "@mui/material";
+import { Cancel } from "@mui/icons-material";
 // PROPS
 import { componentCommonProps } from "../../FormikBuilder/utils/types";
 
-const Tags = ({data, handleDelete}) => {
+const Tags = ({
+  data,
+  handleDelete,
+}: {
+  data: string;
+  handleDelete: (data: string) => void;
+}) => {
   return (
     <Box
       sx={{
@@ -20,19 +28,13 @@ const Tags = ({data, handleDelete}) => {
         color: "#ffffff",
       }}
     >
-      <Stack direction='row' gap={1}>
+      <Stack direction="row" gap={1}>
         <Typography>{data}</Typography>
-        <Cancel
-          sx={{ cursor: "pointer" }}
-          onClick={() => handleDelete(data)}
-        />
+        <Cancel sx={{ cursor: "pointer" }} onClick={() => handleDelete(data)} />
       </Stack>
     </Box>
   );
 };
-
-
-const tagsInitialValue :string[]= [];
 
 export const AddInput = ({
   fieldInfo, // label, options, rows, etc
@@ -40,44 +42,55 @@ export const AddInput = ({
   meta, // touched, error
   setFieldValue, // to use on handleChange
 }: componentCommonProps) => {
-  
   const tagRef = useRef<TextFieldProps>(null);
-  const [tags, setTags] = useState([]);
+  const [tags, setTags] = useState<string[]>([]);
 
-  const handleDelete = (value) => {
-    const newtags = tags.filter((val) => val !== value);
-    setTags(newtags);
+  const handleDelete = (value: string) => {
+    const newTags = tags.filter((val) => val !== value);
+    setTags(newTags);//to try the component without depending on formik
+    setFieldValue(inputProps.name, newTags);
   };
- 
-  const handleOnSubmit = (e) => {
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setTags([...tags, tagRef.current.value]);
-    tagRef.current.value=''
-  }
+    const currentValue: any = tagRef?.current?.value;
+    const newTag: string = typeof currentValue === "string" ? currentValue : "";
+
+    setTags([...tags, newTag]); //to try the component without depending on formik
+    setFieldValue(inputProps.name, [...tags, newTag]);
+  };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <form onSubmit={handleOnSubmit}>
-      <TextField
-        inputRef={tagRef}
-        fullWidth
-        variant="standard"
-        size="small"
-        sx={{ margin: "1rem 0" }}
-        margin="none"
-        placeholder="Enter Tags here"
-        InputProps={{
-          startAdornment: (
-            <Box sx={{ margin: "0 0.2rem 0 0", display: "flex" }}>
-              {tags.map((data, index) => {
-                return (
-                  <Tags data={data} handleDelete={handleDelete} key={index} />
-                );
-              })}
-            </Box>
-          ),
-        }}
-      />
-      </form>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <FieldLabel {...fieldInfo} />
+        <form onSubmit={handleOnSubmit}>
+          <TextField
+            inputRef={tagRef}
+            fullWidth
+            variant="standard"
+            size="small"
+            sx={{ margin: "1rem 0" }}
+            margin="none"
+            placeholder="Enter Tags here"
+            InputProps={{
+              startAdornment: (
+                <Box sx={{ margin: "0 0.2rem 0 0", display: "flex" }}>
+                  {tags.map((data, index) => {
+                    return (
+                      <Tags
+                        data={data}
+                        handleDelete={handleDelete}
+                        key={index}
+                      />
+                    );
+                  })}
+                </Box>
+              ),
+            }}
+          />
+        </form>
+      </Box>
+      <RedErrorBelow meta={meta} />
+    </>
   );
 };
---
+++ src/components/FormikBuilderComponents/materialUI/NumberInput.tsx
@@ -0,0 +1,28 @@
+import { InputNumber } from 'antd';
+import React from 'react';
+import { componentCommonProps } from "../../FormikBuilder/utils/types";
+import { FieldLabel, RedErrorBelow } from "./BasicComponents";
+
+
+
+export const NumberInput = ({
+  fieldInfo, // label, options, rows, etc
+  inputProps, // formik's FieldInputProps (name, value, checked, onBlur) and disabled
+  meta, // touched, error
+  setFieldValue, // to use on handleChange
+}: componentCommonProps) => {
+
+  const customHandle = (e: number) => {
+    console.log(e);
+    setFieldValue(inputProps.name, e);
+  };
+
+  return (
+    <React.Fragment>
+      <FieldLabel {...fieldInfo} />
+      <InputNumber {...inputProps} onChange={customHandle} />
+      <RedErrorBelow meta={meta} />
+    </React.Fragment>
+  );
+};
+
